ea454e395f916399583329ad2d0071808ddacbed
Ruginn Catarata <Ruginn@Yip-Yip.attlocal.net>
2020-10-20 14:35:45 -0400

    these are the files asked for

---
+++ script.js
@@ -0,0 +1,29 @@
+var h3Tag = document.querySelector("h3");
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("gradient");
+var butt = document.getElementById("butt");
+
+
+butt.addEventListener("click", function() {
+    alert("all the girls!");
+})
+
+color1.addEventListener("input", function() {
+    body.style.background = 
+	"linear-gradient(to top, "   
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+    + ")";
+    
+})
+
+color2.addEventListener("input", function() {
+    body.style.background = 
+	"linear-gradient(to top, "   
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+})
